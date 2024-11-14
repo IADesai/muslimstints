cd10de667cbb52dc50ac72e9d88cf5e8c236c151
IADesai <ibrahim.desai02@gmail.com>
2024-11-14 15:08:36 +0000

    Linked Firebase to HTML

--- functions/main.py
+++ functions/main.py
@@ -5,23 +5,12 @@
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+
+
+@https_fn.on_request()
+def on_request_example(req: https_fn.Request) -> https_fn.Response:
+    return https_fn.Response("Hello world!")
 
 # functions/main.py
 
-import functions_framework
-
-@functions_framework.http
-def example_function(request):
-    """Responds to an HTTP request."""
-    request_json = request.get_json(silent=True)
-    if request_json and 'text' in request_json:
-        text = request_json['text']
-        return f"Server received: {text}"
-    else:
-        return "Hello from Python Cloud Function!"
